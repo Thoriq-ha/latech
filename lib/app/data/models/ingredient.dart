67c095c0c2134fbd43fdc799105378038932ac0a
Thoriq-ha <ithoriq0@gmail.com>
2022-11-12 23:29:29 +0700

    adjusting-ui

--- lib/app/data/models/ingredient.dart
+++ lib/app/data/models/ingredient.dart
@@ -1,20 +1,23 @@
 class Ingredient {
   String kode;
   String name;
+  String tipe;
   String desc;
-  String status;
+  List<String> status;
 
   Ingredient(
       {required this.desc,
       required this.kode,
+      required this.tipe,
       required this.name,
       required this.status});
 
   factory Ingredient.fromJson(Map json) {
     return Ingredient(
         desc: json['desc'],
+        tipe: json['tipe'],
         kode: json['kode'],
         name: json['name'],
-        status: json['status']);
+        status: (json['status'] as List).map((e) => e.toString()).toList());
   }
 }
