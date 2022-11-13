8366c6d5da929a3df26d8084b1cf1e82f6677e87
Thoriq-ha <ithoriq0@gmail.com>
2022-11-13 23:01:08 +0700

    update-structure

--- lib/app/modules/main/views/tabs/home/controllers/home_controller.dart
+++ lib/app/modules/main/views/tabs/home/controllers/home_controller.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 import 'package:halalin/app/data/models/product.dart';
-import 'package:halalin/app/services/product_services.dart';
+import 'package:halalin/app/data/services/product_services.dart';
 
 class HomeController extends GetxController {
   RxList<Product> products = RxList();
